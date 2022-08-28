c980582c95987894a50cae1032d5936cfd98ead3
sworc <lightbox@nifty.com>
2022-08-28 22:28:27 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text;
+
+namespace cs_0828_1
+{
+    class Program
+    {
+        private static string fileName = "Settings.dat";
+        static void Main(string[] args)
+        {
+            WriteBinData();
+        }
+
+        private static void WriteBinData()
+        {
+            using (var stream = File.Open(fileName, FileMode.Create))
+            {
+                using (var writer = new BinaryWriter(
+                    stream, Encoding.UTF8, false))
+                {
+                    writer.Write(1.250F);
+                    writer.Write(@"c:\Temp");
+                    writer.Write(10);
+                    writer.Write(true);
+                }
+            }
+        }
+    }
+}
