9738ff62ffdab0647d11cc3b0d0d525c7751019c
sworc <lightbox@nifty.com>
2022-08-28 22:38:33 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,19 +11,5 @@ namespace cs_con_binary_writer
             WriteBinaryData();
         }
 
-        private static void WriteBinaryData()
-        {
-            using (var stream = File.Open(fileName, FileMode.Create))
-            {
-                using (var writer = new BinaryWriter(
-                    stream, Encoding.UTF8, false))
-                {
-                    writer.Write(1.250F);
-                    writer.Write(@"c:\Temp");
-                    writer.Write(10);
-                    writer.Write(true);
-                }
-            }
-        }
     }
 }
